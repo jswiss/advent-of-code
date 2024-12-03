0c8e6cff453f418afb824fd84692f6ff0a4612ab
Joshua Swiss <joshua.swiss@skyscanner.net>
2024-12-03 14:22:58 +0000

    day 1 ✅

--- 2024/internal/days/day1.go
+++ 2024/internal/days/day1.go
@@ -1,12 +1,107 @@
 package days
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"sort"
+	"strconv"
+)
 
 func init() {
 	RegisterDay(1, Run)
 }
 
-// Run is the solution for Day 1
-func Run() {
-	fmt.Println("Solution for Day 1!")
+func SplitLists(list []string) ([]string, []string) {
+	evens := []string{}
+	odds := []string{}
+
+	re := regexp.MustCompile(`(\d+)\s+(\d+)`)
+
+	for _, v := range list {
+		matches := re.FindStringSubmatch(v)
+		if len(matches) > 2 {
+			evens = append(evens, matches[1])
+			odds = append(odds, matches[2])
+
+		} else {
+			fmt.Println("No match found!")
+		}
+	}
+	return evens, odds
 }
+
+func OrderList(list []string) []int {
+	intList := []int{}
+	for _, v := range list {
+		num, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Println("issue with strconv")
+		}
+		intList = append(intList, num)
+	}
+	sort.Ints(intList)
+	return intList
+}
+
+func CalculateDifferences(e []int, o []int) []int {
+	r := []int{}
+
+	if len(e) != len(o) {
+		fmt.Print("Fail!")
+	}
+
+	for i := 0; i < len(e); i++ {
+		diff := e[i] - o[i]
+		if diff < 0 {
+			diff = diff * -1
+		}
+		r = append(r, diff)
+	}
+	return r
+}
+
+func CountOccurrences(e []int, o []int) []int {
+	occ := []int{}
+
+	for _, value := range e {
+		count := 0
+		for _, val := range o {
+			if value == val {
+				count++
+			}
+		}
+		prod := value * count
+		occ = append(occ, prod)
+	}
+	return occ
+}
+
+// Run is the solution for Day 1
+// func Run() {
+// 	lines, err := utils.ReadLines("data/day1-input.txt")
+// 	if err != nil {
+// 		fmt.Print(err)
+// 	}
+
+// 	e, o := SplitLists(lines)
+// 	evens := OrderList(e)
+// 	odds := OrderList(o)
+
+// 	differences := CalculateDifferences(evens, odds)
+
+// 	result := 0
+
+// 	for _, val := range differences {
+// 		result += val
+// 	}
+
+// 	occ := CountOccurrences(evens, odds)
+// 	fmt.Println(occ)
+
+// 	total := 0
+// 	for _, v := range occ {
+// 		total += v
+// 	}
+// 	fmt.Println("Solution for Day 1 part 1 is: ", result)
+// 	fmt.Println("Solution for Day 1 part 2 is: ", total)
+// }
--- 2024/internal/days/day1_test.go
+++ 2024/internal/days/day1_test.go
@@ -1,6 +1,40 @@
 package days
 
-import "testing"
+import (
+	"2024/utils"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+// Splits an array, returning two new slices of even and odd indexed items
+func TestSplitLists(t *testing.T) {
+	expectedEvens := []string{"3", "4", "2", "1", "3", "3"}
+	expectedOdds := []string{"4", "3", "5", "3", "9", "3"}
+
+	lines, _ := utils.ReadLines("data/day1-example-input.txt")
+	evens, odds := SplitLists(lines)
+
+	if reflect.DeepEqual(evens, expectedEvens) {
+		t.Log("Evens don't match")
+	}
+	if reflect.DeepEqual(odds, expectedOdds) {
+		t.Log("Odds don't match")
+	}
+}
+
+func TestOrderList(t *testing.T) {
+	input := []string{"3", "4", "2", "1", "3", "3"}
+	expected := []int{1, 2, 3, 3, 3, 4}
+
+	result := OrderList(input)
+
+	if reflect.DeepEqual(result, expected) {
+		fmt.Println("result and expected do not match")
+		fmt.Println("want: {} {}", expected, reflect.TypeOf(expected))
+		fmt.Println("got: {} {}", result, reflect.TypeOf(result))
+	}
+}
 
 func TestRun(t *testing.T) {
 	// Example test for Day 1 solution
