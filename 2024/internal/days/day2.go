9d4bab89571b0f9a03c2b4862f0f2bf9be4dc01a
Joshua Swiss <joshua.swiss@skyscanner.net>
2024-12-03 14:23:13 +0000

    day2 setup

---
+++ 2024/internal/days/day2.go
@@ -0,0 +1,13 @@
+package days
+
+import "fmt"
+
+func init() {
+	RegisterDay(2, Run)
+}
+
+func Run() {
+
+	fmt.Println("Solution for Day 1 part 1 is: ")
+	fmt.Println("Solution for Day 1 part 2 is: ")
+}
